4c85f89071108478d7436803c1519c61a5065a29
RostamDinyari <r.dinyari.sh@gmail.com>
2024-04-27 09:39:33 -0500

    -all

---
+++ main.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+# Establish a connection to the MongoDB server
+client = MongoClient("mongodb+srv://rdinyarish:<password>@cluster0.jqbyfjn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# Access the database
+db = client["Rework"]
+
+# Access the collection within the database
+collection = db["mainPage"]
